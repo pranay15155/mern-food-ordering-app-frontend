139f283b737cc5c91742469d09be0bf60edc4b03
pranay15155 <pranayprasoon15@gmail.com>
2024-06-27 13:58:28 +0530

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,42 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
+import { BrowserRouter as Router } from "react-router-dom"; // Correct import
+import AppRoutes from "./AppRoutes";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Toaster } from "sonner";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <Router>
+//       <Auth0ProviderWithNavigate>
+//         {" "}
+//         {/* Correct usage of BrowserRouter */}
+//         <AppRoutes />
+//       </Auth0ProviderWithNavigate>
+//     </Router>
+//   </React.StrictMode>
+// );
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+          <Toaster visibleToasts={1} position="top-right" richColors />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
+    </Router>
+  </React.StrictMode>
+);
