82cc805558027419e50632d025bddefc44f64278
pranay15155 <pranayprasoon15@gmail.com>
2024-06-29 06:30:31 +0530

    adding user profile form feature

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -5,7 +5,7 @@ import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
-// import ManageRestaurantPage from "./pages/ManageRestaurantPage";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 // import SearchPage from "./pages/SearchPage";
 // import DetailPage from "./pages/DetailPage";
 // import OrderStatusPage from "./pages/OrderStatusPage";
@@ -15,9 +15,34 @@ import ProtectedRoute from "./auth/ProtectedRoute";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout showHero><HomePage/> </Layout>} />
-      <Route path="/auth-callback" element={<AuthCallbackPage/>} />
-      <Route element={<ProtectedRoute/>}><Route path="/user-profile" element={<Layout><UserProfilePage/></Layout>} /></Route>
+      <Route
+        path="/"
+        element={
+          <Layout showHero>
+            <HomePage />{" "}
+          </Layout>
+        }
+      />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/manage-restaurant"
+          element={
+            <Layout>
+              <ManageRestaurantPage />
+            </Layout>
+          }
+        />
+      </Route>
+
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
