8bbca376abfb089e820c1080d4fb4c851310e4b4
pranay15155 <pranayprasoon15@gmail.com>
2024-07-02 16:20:36 +0530

    pranay changes error

--- src/api/OrderApi.tsx
+++ src/api/OrderApi.tsx
@@ -1,6 +1,6 @@
 // import { Order } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation, useQuery } from "react-query";
+import { useMutation } from "react-query";
 import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
